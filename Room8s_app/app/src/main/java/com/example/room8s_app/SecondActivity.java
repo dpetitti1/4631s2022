35d92e040469f115755684a4a8d5c795a65f6408
dpetitti1 <donna_petitti@student.uml.edu>
2022-04-27 08:42:58 -0400

    updates

--- Room8s_app/app/src/main/java/com/example/room8s_app/SecondActivity.java
+++ Room8s_app/app/src/main/java/com/example/room8s_app/SecondActivity.java
@@ -2,17 +2,13 @@ package com.example.room8s_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import java.text.SimpleDateFormat;
